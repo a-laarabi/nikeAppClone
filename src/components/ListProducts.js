64b4f3c2e7f189cee479c80f7d1faaa855668573
Anasse LAARABI <contact.laarabi@gmail.com>
2023-09-15 19:19:59 +0100

    Update ListProducts.js

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -1,20 +1,32 @@
-import { View, FlatList, Image, StyleSheet } from "react-native";
+import { View, FlatList, Image, StyleSheet, Pressable } from "react-native";
 import React from "react";
-import products from "../data/products";
+// import products from "../data/products";
+import { useNavigation } from '@react-navigation/native';
+import { useDispatch, useSelector } from "react-redux";
+import { productsSlice } from "../store/productSlice";
 
-const ListProducts = () => (
-  <FlatList
+const ListProducts = ({}) => {
+  const navigation = useNavigation();
+  const dispatch = useDispatch();
+  const products = useSelector(state => state.products.products)
+
+  return(
+    <FlatList
     data={products}
     renderItem={({ item }) => {
       return (
-        <View style={styles.product}>
-          <Image source={{ uri: item.image }} style={styles.productImage} />
-        </View>
+        <Pressable onPress={() => {
+            dispatch(productsSlice.actions.setSelectedProduct(item.id));
+            navigation.navigate('Product Detail')
+          }}
+          style={styles.product}>
+            <Image source={{ uri: item.image }} style={styles.productImage} />
+        </Pressable>
       );
     }}
     numColumns={2}
   />
-);
+  )};
 
 const styles = StyleSheet.create({
   product: {
